724e80cce561ee6673baafe7314f486406e134d2
BryceLoski <752358701@qq.com>
2021-09-05 16:07:05 +0800

    version 2.1.2 处理路径为'/design'的HTTP GET请求. 构建配料的列表 处理请求,并将数据返回给要渲染的HTML模板,发送给发起请求的Web浏览器 page30 - page32

--- TacoCloud/src/main/java/com/yzt/tacos/Taco.java
+++ TacoCloud/src/main/java/com/yzt/tacos/Taco.java
@@ -9,13 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //		@SpringBootConfiguration : 声明该类为配置类, 这个注解实际上是 @Configuration 的特殊形式
 //		@EnableAutoConfiguration : 启用Spring Boot的自动配置(自动装配和组件扫描)
 //		@ComponentScan : 启用组件扫描
-public class TacoCloudApplication {
+public class Taco {
 
     public static void main(String[] args) {
         // 这个main()方法会调用SpringApplication中的静态的run()方法,
         // run()方法会执行应用的引导过程,也就是创建Spring的应用上下文,
         // 在传递给run()方法的两个参数中,一个是配置类(.class),一个是命令行参数(args)
-        SpringApplication.run(TacoCloudApplication.class, args);
+        SpringApplication.run(Taco.class, args);
     }
 
 }
