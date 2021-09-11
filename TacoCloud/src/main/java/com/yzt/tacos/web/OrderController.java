9595d20c23b6f93fa2676299e9ae99a678692fa9
BryceLoski <752358701@qq.com>
2021-09-11 17:38:24 +0800

    version 2.5.0 修复了localhost:8080/orders/current页面(ordeForm.html)无法返回数据并找不到页面的bug  -- location: th:action=@{/orders}

--- TacoCloud/src/main/java/com/yzt/tacos/web/OrderController.java
+++ TacoCloud/src/main/java/com/yzt/tacos/web/OrderController.java
@@ -1,5 +1,8 @@
 package com.yzt.tacos.web;
 
+
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
@@ -7,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 import lombok.extern.slf4j.Slf4j;
 import com.yzt.tacos.Order;
 
-import javax.validation.Valid;
 
 @Slf4j
 // 在运行期创建一个SLF4J logger对象
@@ -16,13 +18,13 @@ import javax.validation.Valid;
 // 指明这个控制器的请求处理方法都会处理路径以"/orders"开头的请求
 public class OrderController {
 
-    @GetMapping("/current")
+    @GetMapping(value = "/current")
     // 处理针对"/orders/current"的HTTP GET请求
-    public String OrderForm(Model model){
+    public String orderForm(Model model){
         model.addAttribute("order", new Order());
         return "orderForm";
         // 返回一个名为"orderForm"的逻辑视图名
-        // orderForm 视图 由名为 orderForm.html 的 Thyme leaf 模板提供
+        // orderForm 视图 由名为 orderForm.html 的 Thymeleaf 模板提供
     }
 
     @PostMapping
