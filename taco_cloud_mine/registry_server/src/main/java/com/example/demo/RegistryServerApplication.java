19ac4a02696580eecad8cc679fb1cbe34b675911
BryceLoski <752358701@qq.com>
2021-10-26 13:50:40 +0800

    taco_cloud_mine version 0.0.2 TransportException

--- taco_cloud_mine/registry_server/src/main/java/com/example/demo/RegistryServerApplication.java
+++ taco_cloud_mine/registry_server/src/main/java/com/example/demo/RegistryServerApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ResgistryServerApplication {
+public class RegistryServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ResgistryServerApplication.class, args);
+		SpringApplication.run(RegistryServerApplication.class, args);
 	}
 
 }
